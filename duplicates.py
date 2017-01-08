52b3779fa24df8e9d06572950411681884bf88f6
eremeevfd <dartfedor@mail.ru>
2017-01-08 13:28:31 +0300

    include files with dots, output files as groups, removed str concat

--- duplicates.py
+++ duplicates.py
@@ -7,23 +7,29 @@ def find_duplicate_files(path):
     file_list = {}
     duplicates = defaultdict(list)
     for root, dirs, files in os.walk(path):
-        if '.' not in root:
+        files = [file for file in files if not file[0] == '.']
+        try:
             for filename in files:
                 filepath = os.path.join(root, filename)
                 filesize = os.path.getsize(filepath)
                 filename = os.path.basename(filepath)
-                filename_filesize = filename + ' ' + str(filesize)
+                filename_filesize = '{filename} {filesize}'.format(filename=filename, filesize=filesize)
                 if filename_filesize in file_list:
                     duplicates[filename_filesize].append(filepath)
                     duplicates[filename_filesize].append(file_list.get(filename_filesize))
                 file_list[filename_filesize] = filepath
+        except FileNotFoundError:
+            pass
     return duplicates
 
 
 def print_duplicates(duplicate_files):
     print('This files seem to be duplicates (name, size): ')
-    for number, duplicate in enumerate(duplicate_files.values(), start=1):
-        print(number, duplicate)
+    for duplicate_group in duplicate_files.values():
+        print('--------')
+        for duplicate_file in duplicate_group:
+            print(duplicate_file)
+
 
 if __name__ == '__main__':
     given_path = input("Enter your directory to find duplicate files: ")
